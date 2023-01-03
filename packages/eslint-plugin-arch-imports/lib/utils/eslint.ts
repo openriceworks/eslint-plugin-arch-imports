a4f5b074e45a552c8510a5e1ce233e16f1e97b99
openriceworks <openriceworks@gmail.com>
2023-01-04 00:00:05 +0900

    [fix] docs path

--- packages/eslint-plugin-arch-imports/lib/utils/eslint.ts
+++ packages/eslint-plugin-arch-imports/lib/utils/eslint.ts
@@ -1,5 +1,5 @@
 import { ESLintUtils } from "@typescript-eslint/utils";
 
 export const createRule = ESLintUtils.RuleCreator((ruleName) => {
-  return `https://github.com/openriceworks/eslint-plugin-arch-imports/blob/master/docs/rules/${ruleName}.md`;
+  return `https://github.com/openriceworks/eslint-plugin-arch-imports/blob/main/packages/eslint-plugin-arch-imports/docs/rules/${ruleName}.md`;
 });
